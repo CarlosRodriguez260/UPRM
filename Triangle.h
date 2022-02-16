b1c82c24906784c955fe6e9c1145890c20f4530f
Your Name <you@example.com>
2022-02-16 17:49:44 -0400

    ReStudy

---
+++ Triangle.h
@@ -0,0 +1,6 @@
+class Triangle 
+{
+    double s1;
+    double s2;
+    double s3;
+};
---
+++ lecture1.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+double computeTriangleArea(double s1, double s2, double s3)
+{
+  double s = (s1 + s2 + s3)/2;
+  double area = sqrt(s + (s-s1) * (s-s2) * (s-s3));
+  return area;
+}
+
+// -------------------------------------------------------------------
+string encode(string clearText)
+{
+  string output = "";
+  for(int i=0; i<clearText.size(); i++)
+  {
+    if (isspace(clearText[i]))
+    {
+      output += '_';
+    }
+    else
+    {
+      output += clearText[i];
+    }
+  }
+  return output;
+}
+// --------------------------------------------------------------------
+
+int main() 
+{
+  cout << "Area is: " << computeTriangleArea(4,13,15) << endl;
+  cout << "Hello World Hello -> " << encode("Hello World Hello") << endl;
+}
